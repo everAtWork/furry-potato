5e1dc96cd1f777143f98f6d9152ad0b213a66ea6
everAtWork <ever.at.work@gmail.com>
2021-09-13 00:46:03 +0300

    entities list seems rdy

--- server/models/models.js
+++ server/models/models.js
@@ -9,19 +9,38 @@ const User = sequelize.define('user', {
 })
 
 
-const Basket = sequelize.define('user', {
+const Basket = sequelize.define('basket', {
     id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
 })
 
-const BasketDevice = sequelize.define('user', {
+const BasketDevice = sequelize.define('basket-device', {
     id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
 })
 
 
-const Device = sequelize.define('user', {
+const Device = sequelize.define('device', {
     id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING,unique: true, allowNull: false},
     price: { type: DataTypes.INTEGER, unique: true, allowNull: false},
     rating: { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false},
     img: { type: DataTypes.STRING, allowNull: false}
+})
+
+
+const Type = sequelize.define('type', {
+id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+name: {type: DataTypes.STRING, unique: true, autoIncrement: true}
+})
+const Brand = sequelize.define('brand', {
+id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+name: {type: DataTypes.STRING, unique: true, autoIncrement: true}
+})
+const Rating = sequelize.define('rating', {
+id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+rate: {type: DataTypes.STRING, allowNull: false}
+})
+const DeviceInfo = sequelize.define('device-info', {
+id: {type : DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+title: {type: DataTypes.STRING, allowNull: false},
+description: {type: DataTypes.STRING, allowNull: false}
 })
