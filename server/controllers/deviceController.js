1d4cb723ea5fdb327f6692baccb079a1ba549d12
everAtWork <ever.at.work@gmail.com>
2021-09-20 15:01:07 +0300

    device creation completion

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,21 +1,33 @@
 const uuid = require('uuid')
 const path = require('path')
 const ApiError = require('../error/ApiError')
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const { nextTick } = require('process')
 const { Where } = require('sequelize/types/lib/utils')
+const { JSON } = require('sequelize/types')
 
 
 class DeviceController {
     async create(req,res){
         try {
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
             
             const device = await Device.create({name, price, brandId, typeId, img: fileName})
+            
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i => {
+                    DeviceInfo.create({
+                        title = i.title,
+                        description = i.description,
+                        deviceId = device.id
+                    })
+                });
+            }
+            
             return res.json(device)
             
     } catch (e) {
@@ -23,19 +35,22 @@ class DeviceController {
     }
 }
     async getAll(req,res){
-        const {brandId, typeId} = req.query
+        let {brandId, typeId, limit, page} = req.query // + текущая страница, +лимит
         let devicesGotten // возвращаемое
+        page = page || 1
+        limit = limit || 20 
+        let offset = page * limit - limit
         if (!brandId && !typeId) {
-            devicesGotten = Device.findAll()
+            devicesGotten = Device.findAndCountAll({limit, offset})
         }
         if (brandId && !typeId) {   
-            devicesGotten = Device.findAll({where:{brandId}})
+            devicesGotten = Device.findAndCountAll({where:{brandId}, limit, offset})
         }
         if (!brandId && typeId) {
-            devicesGotten = Device.findAll({where:{typeId}})
+            devicesGotten = Device.findAndCountAll({where:{typeId}, limit, offset})
         }
         if (brandId && typeId) {
-            devicesGotten = Device.findAll({where:{brandId, typeId}})
+            devicesGotten = Device.findAndCountAll({where:{brandId, typeId},limit, offset})
         }
         return res.json(devicesGotten)
     }
