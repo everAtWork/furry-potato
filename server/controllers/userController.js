4799b03a8f119b93431a5849cc411848ff129cd0
everAtWork <ever.at.work@gmail.com>
2021-09-21 12:55:56 +0300

    from token to generateJwt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,24 +3,27 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
 
-
+const generateJwt = (id,email,role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn:'18h'})    
+}
 class UserController {
     async signup(req,res){
         const {email, password,role} = req.body
         if(!email || !password) {
             return next(ApiError.badRequest('your password or email is wrong'))
             const candidate = await User.findOne({where: {email}})
+        }
             if (candidate) {
                 return next(ApiError.badRequest('The email is already in use, wanna login?'))
                 const hashPassword = await bcrypt.hash(password,5)
                 const user = await User.create({role,email,password: hashPassword})
                 const basket = await Basket.create({userId: user.id})
-                const token = jwt.sign(
-                    {id:user.id, email, role},
-                    process.env.SECRET_KEY,
-                    {expiresIn:'18h'})
+                const token = generateJwt(user.id,email,role)
             }
-    }return res.json({token})
+    return res.json({token})
     }
     async login(req,res){
 
