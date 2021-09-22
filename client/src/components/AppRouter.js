9b54d4fa32e3830264b6dfd882cf419c175a7ca1
everAtWork <ever.at.work@gmail.com>
2021-09-22 11:58:56 +0300

    switching between isAuth or not for the routing

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import { authRoutes, publicRoutes } from '../routes'
 export default function AppRouter() {
+    const isAuth = false
     return (
-        <div>
-            AppRouter
-        </div>
+        <Switch>
+            {isAuth && authRoutes.map(({path,Component }) => 
+            <Route key={path} path={path} component={Component} exact/>
+            )}
+            
+            {publicRoutes.map(({path,Component }) => 
+            <Route key={path} path={path} component={Component} exact/>
+            )}
+        </Switch>
     )
 }
