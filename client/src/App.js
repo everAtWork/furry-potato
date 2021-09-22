7d4596204b668408fcf71b2ef61584b2b064728d
everAtWork <ever.at.work@gmail.com>
2021-09-22 11:48:01 +0300

    fixed blunders

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
-      yay
+      <AppRouter/>
     </BrowserRouter>
   );
 }
---
+++ client/src/routes.js
@@ -0,0 +1,13 @@
+import AdminPage from "./pages/AdminPanel"
+
+export const authRoutes = [
+    {
+        path: '/admin',
+        Component: AdminPage
+    }
+]
+
+
+export const publicRoutes = [
+
+]
