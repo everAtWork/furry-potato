710815c4ded1123aa71cc670bf8afbcf5103601f
everAtWork <ever.at.work@gmail.com>
2021-09-22 11:54:30 +0300

    +consts +routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,34 @@
 import AdminPage from "./pages/AdminPanel"
+import Shop from "./pages/Shop"
+import { ADMIN_ROUTE } from "./utils/consts"
 
 export const authRoutes = [
     {
-        path: '/admin',
+        path: ADMIN_ROUTE,
         Component: AdminPage
+    },
+    {
+        path: BASKET_ROUTE,
+        Component: Basket
     }
 ]
 
 
 export const publicRoutes = [
-
+    {
+        path: SHOP_ROUTE,
+        Component: Shop
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Auth
+    },
+    {
+        path: SIGNUP_ROUTE,
+        Component: Auth
+    },
+    {
+        path: DEVICE_ROUTE + '/:id',
+        Component: Device
+    }
 ]
