9d0031be9a5484146c60331101897fd8304d85f4
everAtWork <ever.at.work@gmail.com>
2021-09-22 11:45:32 +0300

    added 'em pages

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+export default function AppRouter() {
+    return (
+        <div>
+            AppRouter
+        </div>
+    )
+}
